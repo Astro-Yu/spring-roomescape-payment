b15d7d6ff15d470807ece41416c2414d92584b4b
yush <yush2202@gmail.com>
2025-06-19 16:07:36 +0900

    build: 초기 설정

---
+++ src/test/resources/sql/ReservationTime.sql
@@ -0,0 +1,22 @@
+insert into reservation_time (start_at)
+values ('10:00');
+insert into reservation_time (start_at)
+values ('11:00');
+insert into reservation_time (start_at)
+values ('12:00');
+insert into reservation_time (start_at)
+values ('13:00');
+insert into reservation_time (start_at)
+values ('14:00');
+insert into reservation_time (start_at)
+values ('15:00');
+insert into reservation_time (start_at)
+values ('16:00');
+insert into reservation_time (start_at)
+values ('17:00');
+insert into reservation_time (start_at)
+values ('18:00');
+insert into reservation_time (start_at)
+values ('19:00');
+insert into reservation_time (start_at)
+values ('20:00');
